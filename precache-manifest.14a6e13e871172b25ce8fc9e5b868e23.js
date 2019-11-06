87c3df165415a4f309b291188a376aa80f11ad16
Yiyao Zhou <zhou0160@algonquinlive.com>
2019-11-06 16:25:33 -0500

    Updates

---
+++ precache-manifest.14a6e13e871172b25ce8fc9e5b868e23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dd693deb41fa707239125e47c1d0dbf",
+    "url": "/mad9135-p1-react-crud-list/index.html"
+  },
+  {
+    "revision": "988ed2c96333924b152f",
+    "url": "/mad9135-p1-react-crud-list/static/css/main.849c296a.chunk.css"
+  },
+  {
+    "revision": "de341c774cabcdfd431e",
+    "url": "/mad9135-p1-react-crud-list/static/js/2.1c11489c.chunk.js"
+  },
+  {
+    "revision": "988ed2c96333924b152f",
+    "url": "/mad9135-p1-react-crud-list/static/js/main.e3d3cfad.chunk.js"
+  },
+  {
+    "revision": "704de676382ea23049f3",
+    "url": "/mad9135-p1-react-crud-list/static/js/runtime-main.96ee9016.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/mad9135-p1-react-crud-list/static/media/logo.25bf045c.svg"
+  }
+]);
